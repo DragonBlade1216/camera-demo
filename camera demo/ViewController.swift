000326d85c2867a09a7fd6845bdc2747d38ed254
BLADEN TAYLOR <rl106568@rlas-macair-21.local>
2018-12-04 10:30:07 -0600

    Initial Commit

---
+++ camera demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera demo
+//
+//  Created by BLADEN TAYLOR on 12/4/18.
+//  Copyright © 2018 BLADEN TAYLOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
